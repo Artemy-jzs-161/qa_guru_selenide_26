96cb9c7a1017e9f506b9d3672f04be4ec4b4ab79
Artemy-jzs-161 <borovik.artemy@yandex.ru>
2024-03-19 22:03:08 +0700

    add test softAssertionsContainsJUnit5

--- src/test/java/github/SelenideSoftAssertionsTest.java
+++ src/test/java/github/SelenideSoftAssertionsTest.java
@@ -5,6 +5,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideSoftAssertionsTest {
@@ -16,10 +18,30 @@ public class SelenideSoftAssertionsTest {
         Configuration.pageLoadStrategy = "eager";
     }
 
-    @Test
-    void softAssertionsContainsJUnit() {
-      open("/selenide/selenide");
-      sleep(4000);
+        @Test
+    void softAssertionsContainsJUnit5() {
+        //Откройте страницу Selenide в Github
+        open("/selenide/selenide");
+        //Перейдите в раздел Wiki проекта
+        $("#wiki-tab").click();
+
+        //Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $("[data-filterable-for=wiki-pages-filter]").$(byText("SoftAssertions")).shouldBe(visible);
+        //Откройте страницу SoftAssertions
+        $("[data-filterable-for=wiki-pages-filter]").$(byText("SoftAssertions")).click();
+        //проверьте что внутри есть пример кода для JUnit5
+        $$("pre").findBy(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}")).shouldBe(visible);
 
     }
 }
