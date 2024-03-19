a5bf518af7d1aa608cebc02c17416364330331d0
Artemy-jzs-161 <borovik.artemy@yandex.ru>
2024-03-19 22:04:30 +0700

    add test softAssertionsContainsJUnit5 fix

--- src/test/java/github/SelenideSoftAssertionsTest.java
+++ src/test/java/github/SelenideSoftAssertionsTest.java
@@ -22,14 +22,17 @@ public class SelenideSoftAssertionsTest {
     void softAssertionsContainsJUnit5() {
         //Откройте страницу Selenide в Github
         open("/selenide/selenide");
+
         //Перейдите в раздел Wiki проекта
         $("#wiki-tab").click();
 
         //Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
         $("#wiki-pages-filter").setValue("SoftAssertions");
         $("[data-filterable-for=wiki-pages-filter]").$(byText("SoftAssertions")).shouldBe(visible);
+
         //Откройте страницу SoftAssertions
         $("[data-filterable-for=wiki-pages-filter]").$(byText("SoftAssertions")).click();
+
         //проверьте что внутри есть пример кода для JUnit5
         $$("pre").findBy(text("@ExtendWith({SoftAssertsExtension.class})\n" +
                 "class Tests {\n" +
